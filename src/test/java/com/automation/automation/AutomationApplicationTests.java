34c0b2ac82539ff3e2a6a9a6e2990b2a81a1aa7b
hmoussa <Trade@KN1234>
2022-07-06 12:04:27 +0200

    initial commit

---
+++ src/test/java/com/automation/automation/AutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automation.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
